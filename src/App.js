13d73bf695325b213c215d99ebc3cb7d29c1fadb
mdraselmiah95 <russelroy15@gmail.com>
2023-01-10 01:03:07 +0600

    Learning React Hook Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+import "./App.css";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors, touchedFields },
+  } = useForm();
+  console.log(watch());
+  console.log("Error-->", errors);
+  console.log(touchedFields);
+
+  const handleForm = (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This is Form. 🔥</h1>
+      <form onSubmit={handleSubmit(handleForm)}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            {...register("name", { required: true })}
+            placeholder="Enter your name..."
+          />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            {...register("email")}
+            placeholder="Enter your email..."
+          />
+        </div>
+
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            name="age"
+            id="age"
+            {...register("age", { valueAsNumber: true, required: true })}
+            placeholder="Enter your age..."
+          />
+        </div>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
